4452ab7733b8917020e5ac346396a4ad5ac3cc1b
Amirhossein Rasoulian <ah.rasoulian@gmail.com>
2021-05-11 20:26:17 +0430

    abstract level of code and functions.

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ def main():
         MSA.append(input())
     long_string = input()
 
+    profile = find_profile(MSA)
+    most_similar_substring = find_most_similar_substring(long_string, profile)
+    print(most_similar_substring)
+
+
+def find_profile(MSA):
+    return ''
+
+
+def find_most_similar_substring(long_string, profile):
+    return ''
+
 
 if __name__ == '__main__':
     main()
